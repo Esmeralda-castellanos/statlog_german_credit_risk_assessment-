634a854a81f9cffaf7f2bef4b84095d8dba0350c
Esmeralda-castellanos <esmeli.ggomez@gmail.com>
2026-06-01 21:28:09 +0200

    Add SQL queries to check row count and preview data

---
+++ 01_row_count_check.sql
@@ -0,0 +1,3 @@
+SELECT * FROM `credit-risk-assessment-497916.credit_risk_db.german_credit_raw` LIMIT 10;
+SELECT COUNT(*) AS total_rows
+FROM `credit-risk-assessment-497916.credit_risk_db.german_credit_raw`;
